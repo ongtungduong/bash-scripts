05517cd1369012ba6d68841319634478dd62edc3
Ông Tùng Dương <97881749+ongtungduong@users.noreply.github.com>
2023-05-31 14:51:32 +0700

    Update install-node-exporter.sh

--- install-node-exporter.sh
+++ install-node-exporter.sh
@@ -3,7 +3,7 @@
 # CHANGE THESE ENVIRONMENT VARIABLES
 EXPORTER_VERSION=1.5.0  # Change me
 EXPORTER_PORT=17017 # Change me
-PASSWORD=QmaQ4K6Tv9 # Change me
+PASSWORD= # Change me
 
 # Download node_exporter
 sudo wget https://github.com/prometheus/node_exporter/releases/download/v$EXPORTER_VERSION/node_exporter-$EXPORTER_VERSION.linux-amd64.tar.gz
@@ -39,6 +39,5 @@ echo "$USER:$PASSWORD" | chpasswd
 
 # Reload systemd daemon and start node_exporter
 sudo systemctl daemon-reload
-sudo systemctl start node_exporter
-sudo systemctl enable node_exporter
+sudo systemctl enable node_exporter --now
 sudo systemctl status node_exporter --no-pager
