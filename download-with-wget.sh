1065638f71d1586250028aa3fdb3e172e3a500a4
Ông Tùng Dương <97881749+ongtungduong@users.noreply.github.com>
2023-05-31 14:54:36 +0700

    Update and rename download-from-s3.sh to download-with-wget.sh

--- download-with-wget.sh
+++ download-with-wget.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Set variables
-FILE_PATH=""    # Change me
-URL=""          # Change me
+URL=""       # Change me
+FOLDER=""    # Change me
 
 # Remove query string from URL and get filename
 url_without_query_string="${URL%%\?*}"
 filename="${url_without_query_string##*/}"
 
 # Download file with wget
-wget $URL -O $FILE_PATH/$filename
+wget $URL -O $FOLDER/$filename
